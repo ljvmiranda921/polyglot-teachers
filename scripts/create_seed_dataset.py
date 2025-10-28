2815f594cd8604d0e576316746fadb2787391410
Lester James Miranda <ljvmiranda@gmail.com>
2025-10-28 12:57:29 +0000

    [wip] Update

--- scripts/create_seed_dataset.py
+++ scripts/create_seed_dataset.py
@@ -1,10 +1,20 @@
 import uuid
+import logging
+import sys
 import argparse
 
 import pandas as pd
 from datasets import Dataset, load_dataset
 
-LANGUAGES = {
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler(sys.stdout)],
+    level=logging.INFO,
+)
+
+LANG_MAPPING = {
     "es": "Spanish",
     "de": "German",
     "id": "Indonesian",
@@ -26,6 +36,39 @@ def get_args():
 def main():
     args = get_args()
 
+    all_dfs = []
+    for dataset_name, processor in DATA_PROCESSORS.items():
+        if dataset_name in args.exclude:
+            logging.info(f"Skipping excluded dataset: {dataset_name}")
+            continue
+        logging.info(f"Processing dataset: {dataset_name}")
+        df = processor()
+
+
+def _process_wildchat() -> pd.DataFrame:
+    num_instances = 200_000
+    wildchat_4_8m = load_dataset("allenai/WildChat-4.8M", split="train", streaming=True)
+    sampled = wildchat_4_8m.shufle(seed=42).take(num_instances)
+
+    sampled_df = pd.DataFrame(list(sampled))
+    filtered_df = sampled_df[(sampled_df["language"].isin(LANG_MAPPING.values()))]
+
+    # Transform to desired format
+    wildchat_df = pd.DataFrame(
+        {
+            "id": [uuid.uuid4().hex for _ in range(len(filtered_df))],
+            "source": "allenai/WildChat-4.8M",
+            "conversation": filtered_df["conversation"].values,
+            "language": filtered_df["language"].map(LANG_MAPPING).values,
+            "strategy": [["generate", "respond"] for _ in range(len(filtered_df))],
+            "source_id": filtered_df["conversation_hash"].values,
+        }
+    )
+
+    breakpoint()
+
+
+DATA_PROCESSORS = {"allenai/WildChat-4.8M": _process_wildchat}
 
 if __name__ == "__main__":
     main()
