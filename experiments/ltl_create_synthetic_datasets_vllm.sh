74b925ce232096fd04edcf9c155c11a43d1817ae
Lester James Miranda <ljvmiranda@gmail.com>
2025-11-03 14:57:02 +0000

    Add initial experiment scripts for slurm

---
+++ experiments/ltl_create_synthetic_datasets_vllm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=synthesize-data
+#! change to gpu:4 to use all 4 GPU cards on a GPU node.
+#SBATCH --nodelist=ltl-gpu05
+#SBATCH --gres=gpu:2
+#SBATCH --time=00:10:00
+#SBATCH --output=gpu-%j.log
+
+# Module setup: cluster environment and recent python.
+module purge
+module load rhel8/default-amp
+
+# Parse arguments
+MODEL=${1:-"meta-llama/Llama-3.1-8B-Instruct"}
+LANGUAGE=${2:-"id"}
+
+source .venv/bin/activate
+.venv/bin/python -m scripts.synthesize_data --help
+.venv/bin/python -m scripts.synthesize_data --input_dataset ljvmiranda921/msde-seed-S1 \
+    --output_dataset ljvmiranda921/test-synthesize-${LANGUAGE} \
+    --target_lang ${LANGUAGE} \
+    --strategy generate \
+    --has_prefilter \
+    --limit 10000 \
+    --backend vllm \
+    --model ${MODEL} \
+    --backend_params '{"max_num_seqs":64,"gpu_utilization":0.7}'
