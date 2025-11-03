f6fbd4147494f207af7fb27f1af2764e7e56e1f7
Lester James Miranda <ljvmiranda@gmail.com>
2025-11-03 10:20:14 +0000

    Add dependencies

--- experiments/create_synthetic_datasets_vllm.sh
+++ experiments/create_synthetic_datasets_vllm.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -l
-#SBATCH --job-name=lj-test
+#SBATCH --job-name=synthesize-data
 #SBATCH --gres=gpu:1,gpu-ram:24G
 #SBATCH --time=00:10:00
 #SBATCH --output=gpu-%j.log
 
-python -m scripts.synthesize_data --help
+# Parse arguments
+MODEL=${1:-"meta-llama/Llama-3.1-8B-Instruct"}
+LANGUAGE=${2:-"id"}
 
-# .venv/bin/python -m scripts.synthesize_data --input_dataset ljvmiranda921/msde-seed-S1 \
-#     --output_dataset ljvmiranda921/test-synthesize-id \
-#     --target_lang id \
-#     --strategy generate \
-#     --has_prefilter \
-#     --limit 1000 \
-#     --backend vllm \
-#     --model meta-llama/Llama-3.1-8B-Instruct
+source .venv/bin/activate
+.venv/bin/python -m scripts.synthesize_data --help
+.venv/bin/python -m scripts.synthesize_data --input_dataset ljvmiranda921/msde-seed-S1 \
+    --output_dataset ljvmiranda921/test-synthesize-${LANGUAGE} \
+    --target_lang ${LANGUAGE} \
+    --strategy generate \
+    --has_prefilter \
+    --limit 10000 \
+    --backend vllm \
+    --model ${MODEL}
