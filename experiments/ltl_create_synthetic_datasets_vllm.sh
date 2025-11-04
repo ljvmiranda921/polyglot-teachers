73ced8ed499caa4ce1ad970d4b28944a77a374be
Lester James Miranda <ljvmiranda@gmail.com>
2025-11-04 08:01:14 +0000

    Parametrize the backend as well

--- experiments/ltl_create_synthetic_datasets_vllm.sh
+++ experiments/ltl_create_synthetic_datasets_vllm.sh
@@ -9,8 +9,9 @@
 
 # Parse arguments
 MODEL=${1:-"meta-llama/Llama-3.1-8B-Instruct"}
-STRATEGY=${2:-"generate"}
-LIMIT=${3:-10000}
+BACKEND=${2:-"vllm"}
+STRATEGY=${3:-"generate"}
+LIMIT=${4:-10000}
 LANGUAGES=(ar cs de es id ja)
 LANGUAGE=${LANGUAGES[SLURM_ARRAY_TASK_ID]} 
 
@@ -22,6 +23,6 @@ python -m scripts.synthesize_data --input_dataset ljvmiranda921/msde-seed-S1 \
     --strategy ${STRATEGY} \
     --has_prefilter \
     --limit ${LIMIT} \
-    --backend vllm \
+    --backend ${BACKEND} \
     --model ${MODEL} \
     --backend_params '{"tensor_parallel_size":2,"gpu_memory_utilization":0.7, "max_model_length":4096, "require_all_responses": false}'
