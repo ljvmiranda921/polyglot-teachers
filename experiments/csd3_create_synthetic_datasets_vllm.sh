622ae56f8c82e1fd030f3fd6ba599d825724c8e5
Lester James Miranda <ljvmiranda@gmail.com>
2025-11-04 07:35:04 +0000

    Update CSD3 command

--- experiments/csd3_create_synthetic_datasets_vllm.sh
+++ experiments/csd3_create_synthetic_datasets_vllm.sh
@@ -17,13 +17,13 @@ MODEL=${1:-"meta-llama/Llama-3.1-8B-Instruct"}
 LANGUAGE=${2:-"id"}
 
 source .venv/bin/activate
-.venv/bin/python -m scripts.synthesize_data --help
-.venv/bin/python -m scripts.synthesize_data --input_dataset ljvmiranda921/msde-seed-S1 \
-    --output_dataset ljvmiranda921/test-synthesize-${LANGUAGE} \
+python -m scripts.synthesize_data --help
+python -m scripts.synthesize_data --input_dataset ljvmiranda921/msde-seed-S1 \
+    --output_dataset ljvmiranda921/msde-S1-${LANGUAGE} \
     --target_lang ${LANGUAGE} \
     --strategy generate \
     --has_prefilter \
     --limit 10000 \
     --backend vllm \
     --model ${MODEL} \
-    --backend_params '{"tensor_parallel_size":2,"gpu_memory_utilization":0.7, "max_model_length":4096}'
+    --backend_params '{"tensor_parallel_size":2,"gpu_memory_utilization":0.7, "max_model_length":4096, "require_all_responses": false}'
