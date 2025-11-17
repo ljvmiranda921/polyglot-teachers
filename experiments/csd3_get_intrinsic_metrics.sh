4c4eee7760f38015ec06309f38279b0c1a73286c
Lester James Miranda <ljvmiranda@gmail.com>
2025-11-17 14:29:21 +0000

    [wip] Update

--- experiments/csd3_get_intrinsic_metrics.sh
+++ experiments/csd3_get_intrinsic_metrics.sh
@@ -7,6 +7,8 @@
 #SBATCH --gres=gpu:2
 #SBATCH --output=gpu-%j.log
 #SBATCH --time=10:00:00
+#! Array computation is 11 models and 6 languages = 66 combinations
+#SBATCH --array=0-65  
 
 # Module setup: cluster environment and recent python.
 . /etc/profile.d/modules.sh 
@@ -35,14 +37,18 @@ MODELS=(
 
 LANGUAGES=(ar cs de es id ja)
 
+MODEL=${MODELS[SLURM_ARRAY_TASK_ID % ${#MODELS[@]}]}
+LANGUAGE=${LANGUAGES[SLURM_ARRAY_TASK_ID / ${#MODELS[@]}]}
+
+echo "Computing intrinsic metrics for model: ${MODEL} and language: ${LANGUAGE}"
 
 source .venv/bin/activate
 python -m scripts.get_intrinsic_metrics --help
 
 # Compute across models (get 3.5k samples for each strategy)
-python -m scripts.get_intrinsic_metrics --input_dataset ljvmiranda921/msde-S1-ar \
+python -m scripts.get_intrinsic_metrics --input_dataset ljvmiranda921/msde-S1-${LANGUAGE} \
     --metrics all \
-    --output_path /home/ljvm2/rds/hpc-work/multilingual-teacher-eval/metrics/msde-S1-ar_intrinsic_metrics.json \
-    --metric_params '{"reward_model::{"language":"ar", "tensor_parallel_size": 2}}' \
-    --input_dataset_filter '{"model": ${MODEL}}' \
+    --output_path /home/ljvm2/rds/hpc-work/multilingual-teacher-eval/metrics/msde-S1-${LANGUAGE}_intrinsic_metrics.json \
+    --metric_params '{"reward_model::":{"language":"'"${LANGUAGE}"'","tensor_parallel_size":2}}' \
+    --input_dataset_filter '{"model":"'"${MODEL}"'"}' \
     --apply_subsampling 
