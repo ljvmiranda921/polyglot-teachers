f1a968f5620495653beabded9e9f657b9d82b9ec
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-01 22:52:23 +0000

    [wip] Update

--- scripts/lighteval_tasks.py
+++ scripts/lighteval_tasks.py
@@ -64,51 +64,28 @@ GLOBAL_MMLU_LITE = [
 ]
 
 
-def get_judge_prompt_mrewardbench(question, answer, gold, **kwargs):
-    MREWARDBENCH_TEMPLATE = ""
-    content = MREWARDBENCH_TEMPLATE.format(
-        question=question,
-        target=gold,
-        predicted_answer=answer,
-    )
-    return [{"role": "user", "content": content}]
-
-
-def process_judge_response_mrewardbench(response: str) -> float:
-    if response == "A":
-        return 1.0
-    elif response == "B":
-        return 0.0
-    else:
-        logging.warning(f"Unknown response from judge: {response}")
-        return 0.0
-
-
-class JudgeMRewardBench(JudgeLLM):
-    def __init__(self):
-        # TODO: update this. use as reference: https://github.com/huggingface/lighteval/blob/main/src/lighteval/metrics/metrics_sample.py#L1011
-        super().__init__(
-            judge_model_name="gpt-4o-2024-08-06",
-            template=get_judge_prompt_mrewardbench,
-            process_judge_response=process_judge_response_mrewardbench,
-            judge_backend="openai",
-            short_judge_name="gpt4o",
-        )
-
-    def compute(
-        self, responses: list[ModelResponse], docs: list[Doc], **kwargs
-    ) -> list:
-        # TODO
-        pass
-
-
-# the judge is then passed on to the SampleMetric: https://github.com/huggingface/lighteval/blob/99ef5b98d422cf3620eebec9db13285493d35542/src/lighteval/metrics/metrics.py#L553C1-L562C6
-# which is then passed on to the metrics in the LightEvaltask config: https://github.com/huggingface/lighteval/blob/99ef5b98d422cf3620eebec9db13285493d35542/src/lighteval/tasks/tasks/mt_bench/main.py#L82
-# but it's kinda weird because you see something like this: https://github.com/huggingface/lighteval/blob/99ef5b98d422cf3620eebec9db13285493d35542/src/lighteval/tasks/tasks/mt_bench/main.py#L38
 # IGNORE EVERYTHING ABOVE. I THINK THIS IS JUST MCQ. what the others did is that an LM made a response, and then it's judged.
 # FOR US IT"S JUST MCQ!! SO SIMPLE! don't do the things above!!
 
-M_REWARDBENCH = []
+M_REWARDBENCH = [
+    LightevalTaskConfig(
+        name=f"mrewardbench:{standardize_tag(language.value)}",
+        prompt_function=get_mcq_prompt_function(
+            language,
+            lambda line: {
+                # TODO: follow this https://github.com/filbench/lighteval/blob/main/filbench/sib200.py
+            },
+        ),
+    )
+    for language in [
+        Language.ARABIC,
+        Language.CZECH,
+        Language.GERMAN,
+        Language.SPANISH,
+        Language.INDONESIAN,
+        Language.JAPANESE,
+    ]
+]
 
 
 TASKS_TABLE: list[LightevalTaskConfig] = GLOBAL_MMLU_LITE
