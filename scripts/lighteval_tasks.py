9ee9a82db1646bfd51bc8e689a9ec45286239480
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-01 21:37:50 +0000

    [wip] Update

--- scripts/lighteval_tasks.py
+++ scripts/lighteval_tasks.py
@@ -5,16 +5,15 @@ from lighteval.metrics.dynamic_metrics import loglikelihood_acc_metric
 from lighteval.metrics.normalizations import LogProbCharNorm, LogProbPMINorm, LogProbTokenNorm  # fmt: skip
 from lighteval.tasks.default_prompts import LETTER_INDICES
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
-from lighteval.tasks.multilingual.tasks import MMLU_SUBSETS
 from lighteval.tasks.multilingual.utils.task_utils import get_metrics_for_formulation
 from lighteval.tasks.templates.multichoice import get_mcq_prompt_function
-from lighteval.tasks.templates.utils.formulation import CFFormulation, HybridFormulation, MCFFormulation # fmt: skip
+from lighteval.tasks.templates.utils.formulation import MCFFormulation
 from lighteval.utils.language import Language
 
 
 GLOBAL_MMLU_LITE = [
     LightevalTaskConfig(
-        name=f"global_mmlu_{sensitivity_label.lower()}_{language.value}_{formulation.name.lower()}:{subset}",
+        name=f"global_mmlu_lite_{language.value}_mcf",
         prompt_function=get_mcq_prompt_function(
             language,
             lambda line: {
@@ -27,22 +26,15 @@ GLOBAL_MMLU_LITE = [
                 ],
                 "gold_idx": LETTER_INDICES.index(line["answer"]),
             },
-            formulation=formulation,
+            formulation=MCFFormulation(),
         ),
         suite=("community",),
         hf_repo="CohereForAI/Global-MMLU-Lite",
         hf_subset=standardize_tag(language.value),
         evaluation_splits=("test",),
         few_shots_split="dev",
-        hf_filter=partial(
-            lambda subset, sensitivity_label, x: x["subject"].lower() == subset
-            and sensitivity_label
-            in x["cultural_sensitivity_label"].replace("-", "UNK"),
-            subset,
-            sensitivity_label,
-        ),
         metric=get_metrics_for_formulation(
-            formulation,
+            MCFFormulation(),
             [
                 loglikelihood_acc_metric(normalization=LogProbTokenNorm()),
                 loglikelihood_acc_metric(normalization=LogProbCharNorm()),
@@ -50,7 +42,6 @@ GLOBAL_MMLU_LITE = [
             ],
         ),
     )
-    for subset in MMLU_SUBSETS
     for language in [
         Language.ARABIC,
         Language.GERMAN,
@@ -58,8 +49,6 @@ GLOBAL_MMLU_LITE = [
         Language.INDONESIAN,
         Language.JAPANESE,
     ]
-    for formulation in [MCFFormulation(), CFFormulation(), HybridFormulation()]
-    for sensitivity_label in ["CA", "CS"]
 ]
 
 TASKS_TABLE: list[LightevalTaskConfig] = [
