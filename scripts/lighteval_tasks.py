737e74783dea45680fee5852eb5a74bc6ad5a0c0
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-01 22:34:49 +0000

    [wip] Update

--- scripts/lighteval_tasks.py
+++ scripts/lighteval_tasks.py
@@ -1,14 +1,26 @@
-from functools import partial
+import logging
+import sys
 
 from langcodes import standardize_tag
 from lighteval.metrics.dynamic_metrics import loglikelihood_acc_metric
-from lighteval.metrics.normalizations import LogProbCharNorm, LogProbPMINorm, LogProbTokenNorm  # fmt: skip
+from lighteval.metrics.metrics_sample import JudgeLLM
+from lighteval.metrics.normalizations import LogProbCharNorm  # fmt: skip
+from lighteval.metrics.normalizations import LogProbPMINorm, LogProbTokenNorm
+from lighteval.models.model_output import ModelResponse
 from lighteval.tasks.default_prompts import LETTER_INDICES
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 from lighteval.tasks.multilingual.utils.task_utils import get_metrics_for_formulation
+from lighteval.tasks.requests import Doc
 from lighteval.tasks.templates.multichoice import get_mcq_prompt_function
 from lighteval.tasks.templates.utils.formulation import MCFFormulation
 from lighteval.utils.language import Language
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler(sys.stdout)],
+    level=logging.INFO,
+)
 
 
 GLOBAL_MMLU_LITE = [
@@ -51,7 +63,38 @@ GLOBAL_MMLU_LITE = [
     ]
 ]
 
-M_GSM = []
+
+def get_judge_prompt_mrewardbench():
+    pass
+
+
+def process_judge_response_mrewardbench(response: str) -> float:
+    if response == "A":
+        return 1.0
+    elif response == "B":
+        return 0.0
+    else:
+        logging.warning(f"Unknown response from judge: {response}")
+        return 0.0
+
+
+class JudgeMRewardBench(JudgeLLM):
+    def __init__(self):
+        super().__init__(
+            judge_model_name="gpt-4o-2024-08-06",
+            template=get_judge_prompt_mrewardbench,
+            process_judge_response=process_judge_response_mrewardbench,
+            judge_backend="openai",
+            short_judge_name="gpt4o",
+        )
+
+    def compute(
+        self, responses: list[ModelResponse], docs: list[Doc], **kwargs
+    ) -> list:
+        pass
+
+
+M_REWARDBENCH = []
 
 
 TASKS_TABLE: list[LightevalTaskConfig] = GLOBAL_MMLU_LITE
