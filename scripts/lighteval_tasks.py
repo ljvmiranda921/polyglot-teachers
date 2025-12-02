bd2d3dcccf6d2f1a55b6532c91c3ac098d25b4c4
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-02 09:48:37 +0000

    [wip] Update

--- scripts/lighteval_tasks.py
+++ scripts/lighteval_tasks.py
@@ -23,6 +23,8 @@ logging.basicConfig(
 )
 
 
+# ==== Global-MMU-Lite ====
+
 GLOBAL_MMLU_LITE = [
     LightevalTaskConfig(
         name=f"global_mmlu_lite:{standardize_tag(language.value)}",
@@ -67,10 +69,43 @@ GLOBAL_MMLU_LITE = [
 # from last night LJ to morning LJ: IGNORE EVERYTHING ABOVE. I THINK THIS IS JUST MCQ. what the others did is that an LM made a response, and then it's judged.
 # FOR US IT"S JUST MCQ!! SO SIMPLE! ignore everything above!!
 
+# ==== M-RewardBench ====
+
 
 def get_mrewardbench_eval_instances(line: dict) -> dict[str, Any]:
     """Returns an eval instance of M-RewardBench containing the question, choices, and gold index."""
-    pass
+    # Reference: https://github.com/Cohere-Labs-Community/m-rewardbench/blob/main/scripts/generative.py#L218
+    PROMPT_TEMPLATE = (
+        "Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user question displayed below. "  # noqa
+        "The question provided is in {src_lang}. "  # noqa
+        "You should choose the assistant that follows the user's instructions and answers the user's question better. "  # noqa
+        "Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of their responses. "  # noqa
+        "Also, make sure that the assistant responses are in {tgt_lang}. "  # noqa
+        "Begin your evaluation by comparing the two responses and provide a short explanation. "  # noqa
+        "Avoid any position biases and ensure that the order in which the responses were presented does not influence your decision. "  # noqa
+        "Do not allow the length of the responses to influence your evaluation. "  # noqa
+        "Do not favor certain names of the assistants. "  # noqa
+        "Be as objective as possible. "  # noqa
+        "After providing your explanation, output your final verdict by strictly following this format: "  # noqa
+        '"A" if assistant A is better, "B" if assistant B is better.'  # noqa, removed tie option as , and \"[[C]]\ " for a tie
+        "Don't put any quotation marks around your final verdict. "  # noqa
+        "Here is the user question: {question} "  # noqa
+        "Here is assistant A's response: {assistant_a_response} "  # noqa
+        "Here is assistant B's response: {assistant_b_response} "  # noqa
+    )
+
+    chosen_response = line["chosen"]
+    rejected_response = line["rejected"]
+
+    PROMPT_TEMPLATE.format(
+        src_lang=line["source_language"],
+        tgt_lang=line["target_language"],
+        question=line["prompt"],
+        assistant_a_response=chosen_response,
+        assistant_b_response=rejected_response,
+    )
+
+    return {"question": question, "choices": choices, "gold_idx": gold_idx}
 
 
 def iso2_to_extended_format(iso2_code: str) -> str:
