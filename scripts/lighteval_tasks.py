9ebadc334f89349844b6cb7564db3fa26569e1ef
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-02 09:39:16 +0000

    [wip] Update

--- scripts/lighteval_tasks.py
+++ scripts/lighteval_tasks.py
@@ -1,16 +1,16 @@
 import logging
 import sys
+from typing import Any
+import random
 
 from langcodes import standardize_tag
+from langcodes import Language as LangCodeLanguage
 from lighteval.metrics.dynamic_metrics import loglikelihood_acc_metric
-from lighteval.metrics.metrics_sample import JudgeLLM
 from lighteval.metrics.normalizations import LogProbCharNorm  # fmt: skip
 from lighteval.metrics.normalizations import LogProbPMINorm, LogProbTokenNorm
-from lighteval.models.model_output import ModelResponse
 from lighteval.tasks.default_prompts import LETTER_INDICES
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 from lighteval.tasks.multilingual.utils.task_utils import get_metrics_for_formulation
-from lighteval.tasks.requests import Doc
 from lighteval.tasks.templates.multichoice import get_mcq_prompt_function
 from lighteval.tasks.templates.utils.formulation import MCFFormulation
 from lighteval.utils.language import Language
@@ -64,8 +64,23 @@ GLOBAL_MMLU_LITE = [
 ]
 
 
-# IGNORE EVERYTHING ABOVE. I THINK THIS IS JUST MCQ. what the others did is that an LM made a response, and then it's judged.
-# FOR US IT"S JUST MCQ!! SO SIMPLE! don't do the things above!!
+# from last night LJ to morning LJ: IGNORE EVERYTHING ABOVE. I THINK THIS IS JUST MCQ. what the others did is that an LM made a response, and then it's judged.
+# FOR US IT"S JUST MCQ!! SO SIMPLE! ignore everything above!!
+
+
+def get_mrewardbench_eval_instances(line: dict) -> dict[str, Any]:
+    """Returns an eval instance of M-RewardBench containing the question, choices, and gold index."""
+    pass
+
+
+def iso2_to_extended_format(iso2_code: str) -> str:
+    """Convert ISO 639-1 (2-letter) language code to extended format with script."""
+    lang = LangCodeLanguage.get(iso2_code)
+    maximized = lang.maximize()
+    alpha3 = lang.to_alpha3()
+    script = maximized.script
+    return f"{alpha3}_{script}" if script else alpha3
+
 
 M_REWARDBENCH = [
     LightevalTaskConfig(
@@ -75,7 +90,13 @@ M_REWARDBENCH = [
             lambda line: {
                 # TODO: follow this https://github.com/filbench/lighteval/blob/main/filbench/sib200.py
             },
+            formulation=MCFFormulation(),
         ),
+        suite=("lighteval",),
+        hf_repo="CohereLabsCommunity/multilingual-reward-bench",
+        hf_subset=iso2_to_extended_format(standardize_tag(language.value)),
+        evaluation_splits=("test",),
+        few_shots_split="test",
     )
     for language in [
         Language.ARABIC,
