509154ae93ad3c5daa9f433a237ad23479ffb2cb
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-03 10:12:36 +0000

    [wip] Update

--- scripts/finetune_tunix.py
+++ scripts/finetune_tunix.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import logging
+from pathlib import Path
 
 import jax
 import numpy as np
@@ -32,16 +33,24 @@ def get_args():
     parser.add_argument("--chat_template", type=str, choices=list(CHAT_TEMPLATES.keys()), default="llama-3.1", help="Chat template to use for formatting the messages.")
     parser.add_argument("--output_model_name", type=str, default="ljvmiranda921/msde-sft-dev", help="Name of the output model (HuggingFace ID) to save after finetuning.")
     parser.add_argument("--learning_rate", type=float, default=5e-6, help="Learning rate for finetuning.")
+    parser.add_argument("--batch_size", type=int, default=8, help="Batch size for finetuning.")
     parser.add_argument("--num_epochs", type=int, default=3, help="Number of epochs to finetune for.")
     parser.add_argument("--max_seq_length", type=int, default=2048, help="Maximum sequence length for the model.")
     parser.add_argument("--use_lora", action="store_true", help="If set, will use LoRA for finetuning.")
+    parser.add_argument("--checkpoints_dir", type=str, default="./checkpoints", help="Directory to save checkpoints.")
     parser.add_argument("--seed", type=int, default=3407, help="Random seed for reproducibility.")
     # fmt: on
     return parser.parse_args()
 
 
 def main():
-    pass
+    args = get_args()
+
+    checkpoints_dir = {
+        "full_ckpt": Path(args.checkpoints_dir) / "full_ckpts",
+        "lora_ckpt": Path(args.checkpoints_dir) / "lora_ckpts",
+        "profiling": Path(args.checkpoints_dir) / "profiling",
+    }
 
 
 def get_device_info() -> list:
