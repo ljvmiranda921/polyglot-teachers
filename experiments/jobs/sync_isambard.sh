aaa910dc04cd8551aff6de1bed31c9083f69efe7
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-11 21:19:40 +0000

    [wip] Update

--- experiments/jobs/sync_isambard.sh
+++ experiments/jobs/sync_isambard.sh
@@ -1,6 +1,6 @@
 # Patch the installation of torch in the compute node (so that it downloads the CUDA version).
 # The login node doesn't have the CUDA libraries available.
-uv pip install --reinstall torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
-uv pip install --reinstall triton
-uv pip install --reinstall -U vllm --torch-backend=auto --extra-index-url https://wheels.vllm.ai/0.10.2/vllm
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+uv pip install triton
+uv pip install -U vllm --torch-backend=auto --extra-index-url https://wheels.vllm.ai/0.10.2/vllm
 python -c "import torch; print(torch.cuda.is_available())"
