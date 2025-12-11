b802f7379ef2058742233ac2bb1100d63e779394
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-11 09:15:47 +0000

    Attempt to fix the gsm8k eval setup

--- experiments/jobs/eval_teacher_models.sh
+++ experiments/jobs/eval_teacher_models.sh
@@ -9,9 +9,9 @@ TASKS=(
     "lighteval|mrewardbench_mcf:de|0|0"
     "lighteval|mrewardbench_mcf:es|0|0"
     "lighteval|mrewardbench_mcf:ja|0|0"
-    "lighteval|mgsm:de|0|0"
-    "lighteval|mgsm:es|0|0"
-    "lighteval|mgsm:ja|0|0"
+    "lighteval|mgsm:de|5|0"
+    "lighteval|mgsm:es|5|0"
+    "lighteval|mgsm:ja|5|0"
     "lighteval|global_mmlu_lite:ar|0|0"
     "lighteval|mrewardbench_mcf:ar|0|0"
     "lighteval|global_mmlu_lite:id|0|0"
@@ -36,14 +36,14 @@ MODEL=${MODELS[SLURM_ARRAY_TASK_ID / ${#TASKS[@]}]}
 
 echo "Evaluating model: ${MODEL} on task: ${TASK}"
 
+# Reference for gsm8k setup: https://github.com/huggingface/lighteval/issues/686
 source .venv/bin/activate
 lighteval vllm --help
-lighteval vllm "model_name=${MODEL},tensor_parallel_size=2,gpu_memory_utilization=0.9,max_model_len=16384" "${TASK}" \
+lighteval vllm "model_name=${MODEL},tensor_parallel_size=2,gpu_memory_utilization=0.9,max_model_length=4096,dtype=bfloat16,generation_parameters={max_new_tokens:4096,temperature:0.6,top_p:0.95}" "${TASK}" \
     --custom-tasks scripts/lighteval_tasks.py \
     --output-dir lighteval-results \
     --results-path-template '{output_dir}/{org}___{model}' \
     --use-chat-template \
-    --system-prompt "You are a fair judge assistant tasked with providing clear, objective feedback based on specific criteria, ensuring each assessment reflects the absolute standards set for performance." \
     --no-public-run \
     --results-org "ljvmiranda921" \
     --push-to-hub \
