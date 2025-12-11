dc7616f2d18a825e92e20ce6401e800580953fe4
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-11 22:23:26 +0000

    Fix lighteval setup and use modern version

--- experiments/jobs/sync_isambard.sh
+++ experiments/jobs/sync_isambard.sh
@@ -3,5 +3,5 @@
 uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 uv pip install triton
 uv pip install -U vllm --torch-backend=auto --extra-index-url https://wheels.vllm.ai/0.10.2/vllm
-uv pip install lighteval==0.10.0 datasets==3.6.0
+uv pip install git+https://github.com/ljvmiranda921/lighteval.git@mtep-main 
 python -c "import torch; print(torch.cuda.is_available())"
