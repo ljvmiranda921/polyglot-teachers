f7b935b62dd41e7792afbd7b29116da46fa051e8
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-12 16:38:22 +0000

    Use cu124 for better compatibility of drivers

--- experiments/jobs/sync_isambard.sh
+++ experiments/jobs/sync_isambard.sh
@@ -1,7 +1,7 @@
 # Patch the installation of torch in the compute node (so that it downloads the CUDA version).
 # The login node doesn't have the CUDA libraries available.
-uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+uv pip install torch torchvision torchaudio torchao --index-url https://download.pytorch.org/whl/cu124
 uv pip install triton
 uv pip install -U vllm --torch-backend=auto --extra-index-url https://wheels.vllm.ai/0.10.2/vllm
 uv pip install -e ./lighteval
-python -c "import torch; print(torch.cuda.is_available())"
+python -c "import torch; print(torch.cuda.is_available()); print(torch.__version__)"
