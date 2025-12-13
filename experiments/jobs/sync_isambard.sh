13b240f447f04df8eead3143734e6885b812e1ee
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-13 10:27:25 +0000

    [wip] Update

--- experiments/jobs/sync_isambard.sh
+++ experiments/jobs/sync_isambard.sh
@@ -3,5 +3,6 @@
 uv pip install torch torchvision torchaudio torchao --index-url https://download.pytorch.org/whl/cu124
 uv pip install triton
 uv pip install -U vllm --torch-backend=auto --extra-index-url https://wheels.vllm.ai/0.10.2/vllm
+uv pip install -U "vllm>=0.10.0,<=0.11.2" --torch-backend=auto --index-url https://wheels.vllm.ai/0.11.2/vllm
 uv pip install -e ./lighteval
 python -c "import torch; print(torch.cuda.is_available()); print(torch.__version__)"
