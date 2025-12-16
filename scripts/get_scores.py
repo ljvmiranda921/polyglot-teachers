2694244f2dc5a629e0e01c49cede73c20004462d
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-16 08:29:36 +0000

    [wip] Update

---
+++ scripts/get_scores.py
@@ -0,0 +1,18 @@
+import argparse
+from pathlib import Path
+
+import pandas as pd
+from huggingface_hub import hf_hub_download
+from datasets import load_dataset
+
+
+def get_args():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
