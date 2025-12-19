c115989db8d6be962c34b051ee9c988de447cd2c
Lester James Miranda <ljvmiranda@gmail.com>
2025-12-19 09:55:43 +0000

    [wip] Update

--- plot/model_scale.py
+++ plot/model_scale.py
@@ -1,6 +1,23 @@
 import argparse
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+from plot.utils.plot_theme import PLOT_PARAMS, COLORS
+
+plt.rcParams.update(PLOT_PARAMS)
+
+
+def get_args():
+    # fmt: off
+    # fmt: on
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
