6efef3999c54839cf7048a37512fac77e13b429c
Lester James Miranda <ljvmiranda@gmail.com>
2026-01-02 15:24:54 +0000

    [wip] Update

--- scripts/get_scores.py
+++ scripts/get_scores.py
@@ -49,6 +49,7 @@ def get_args():
     parser.add_argument("--extrinsic_kwargs", type=str, default="{}", help="Extra arguments to pass when processing the extrinsic metrics.")
     parser.add_argument("--ref_model_results", type=str, default="ljvmiranda921/details_allenai__Olmo-3-7B-Instruct-SFT_private", help="Huggingface Dataset containing the reference model results.")
     parser.add_argument("--base_model_results", type=str, default="ljvmiranda921/details_allenai__Olmo-3-1025-7B_private", help="Huggingface Dataset containing the base model results.")
+    parser.add_argument("--show_per_language", action="store_true", help="Whether to show per-language PG-Scores.")
     # fmt: on
     return parser.parse_args()
 
@@ -97,9 +98,12 @@ def main():
     df_merged["pg_score"] = (df_merged["pgr"] + df_merged["z_score"]) / 2
 
     print("\n====== PGR (grouped-by teacher model across languages) ======")
+    aggregator_dict = {"pg_score": "mean"}
+    if args.show_per_language:
+        aggregator_dict.update({"target_lang": "mean"})
     print(
         df_merged.groupby("teacher_model")
-        .agg({"pg_score": "mean"})
+        .agg(aggregator_dict)
         .reset_index()
         .to_markdown(index=False)
     )
